abd1d013ce885a3f9fdda90b15d2fc31dd21ba1e
AttiaMinAllah <atiyaa30@gmail.com>
2024-10-07 12:08:25 +0200

    Added Input text test

---
+++ tests/input-test.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Text Input Tests', () => {
+  test('should update the button label after entering text and clicking the button', async ({ page }) => {
+    await page.goto('http://uitestingplayground.com/textinput');
+
+    const inputField = page.locator('#newButtonName');
+    const button = page.locator('button#updatingButton');
+
+    const newButtonName = 'New Button Test CTA ';
+    await inputField.fill(newButtonName);
+    await button.click();
+
+    await expect(button).toHaveText(newButtonName);
+  });
+});
